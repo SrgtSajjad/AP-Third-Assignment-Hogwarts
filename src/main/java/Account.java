923b863e4462cc3883d77f758f5519e27c3c3468
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-13 17:41:19 +0330

    Account class attributes updated

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -3,18 +3,61 @@ import java.util.UUID;
 public class Account implements AccountManagement {
     private String username;
     private String password;
+    private String role;
+    private boolean approved;
     private UUID accountID;
+
+    public Account(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        accountID = UUID.randomUUID();
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+
+    public UUID getAccountID() {
+        return accountID;
+    }
 
     @Override
     public boolean validatePassword(String enteredPassword) {
+        if (this.password == enteredPassword)
+            return true;
         return false;
     }
 
     @Override
     public void changeUsername(String newUsername) {
+        this.username = newUsername;
     }
 
     @Override
     public void changePassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
