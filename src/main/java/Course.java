decdadbea3c3cea64d0cc9c9c9d271508a0fd47a
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-14 20:03:18 +0330

    create assignment attribute in Course class

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -5,11 +5,17 @@ public class Course {
     String title;
     Teacher teacher;
     UUID courseID;
+    private String assignment;
     ArrayList<Student> participatingStudents = new ArrayList<Student>();
 
     public Course(String title, Teacher teacher) {
         this.title = title;
         this.teacher = teacher;
+        assignment = "No assignments";
         courseID = UUID.randomUUID();
+    }
+
+    public void setAssignment(String assignment) {
+        this.assignment = assignment;
     }
 }
