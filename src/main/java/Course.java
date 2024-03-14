857b291fce8531cb3a801c403d81d93d3b13d2a5
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-14 12:22:15 +0330

    course class updated

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Course {
+    String title;
+    Teacher teacher;
+    UUID courseID;
+    static ArrayList<Student> participatingStudents = new ArrayList<Student>();
+
+    public Course(String title, Teacher teacher) {
+        this.title = title;
+        this.teacher = teacher;
+        courseID = UUID.randomUUID();
+    }
 }
