d4deb9c4f41ad30f61733412a0a60180f5976738
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-14 14:12:51 +0330

    Course class attributes updated

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -5,7 +5,7 @@ public class Course {
     String title;
     Teacher teacher;
     UUID courseID;
-    static ArrayList<Student> participatingStudents = new ArrayList<Student>();
+    ArrayList<Student> participatingStudents = new ArrayList<Student>();
 
     public Course(String title, Teacher teacher) {
         this.title = title;
