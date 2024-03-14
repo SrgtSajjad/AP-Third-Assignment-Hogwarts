f3d893b657d8be47f3591bdc3aa7847362904bc9
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-14 09:08:11 +0330

    Student class created and extended Educational class

---
+++ src/main/java/Student.java
@@ -0,0 +1,10 @@
+public class Student extends Educational {
+    public Student(String username, String password, String role) {
+        super(username, password, role);
+    }
+
+    public void takeCourses() {
+
+    }
+
+}
