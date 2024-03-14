d56327bfd030d2c7bcd346a640179c8a0b7c81db
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-14 14:11:53 +0330

    Student class attributes updated

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+
 public class Student extends Educational {
+
+    ArrayList<Course> coursesTaken = new ArrayList<Course>();
+    ArrayList<Integer> scores = new ArrayList<Integer>();
     public Student(String username, String password, String role) {
         super(username, password, role);
     }
 
-    public void takeCourses() {
 
-    }
 
 }
