e9088331bc2aa4c08d4ade4eb13a6d1a47fea488
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-14 08:43:14 +0330

    Teacher class exended from Educational class

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,2 +1,23 @@
-public class Teacher {
+public class Teacher extends Educational {
+
+    public Teacher(String username, String password, String role) {
+        super(username, password, role);
+    }
+
+    @Override
+    public void signUp() {
+
+    }
+
+    @Override
+    public void takeCourses() {
+
+    }
+
+    @Override
+    public void takeSortingQuiz() {
+
+    }
+
 }
+
