eda5d3f2adc5934074095ef28850377f0abbe609
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-16 02:19:36 +0330

    complete TODO methods

--- src/main/java/Hogwarts.java
+++ src/main/java/Hogwarts.java
@@ -1,19 +1,33 @@
-public class Hogwarts {
+public class Hogwarts extends Account {
+    public Hogwarts(String username, String password, String role) {
+        super(username, password, role);
+    }
 
     // TODO: Define Attributes
 
 
-
     // TODO: Define Functionalities
     public void viewAllTeachers() {
-        //TODO
+        int i = 0;
+        for (Teacher teacher : Educational.teachers) {
+            i++;
+            System.out.println(i + ". " + teacher.getFullName());
+        }
     }
 
     public void viewAllStudents() {
-        //TODO
+        int i = 0;
+        for (Student student : Educational.students) {
+            i++;
+            System.out.println(i + ". " + student.getFullName());
+        }
     }
 
     public void viewAllCourses() {
-        //TODO
+        int i = 0;
+        for (Course course : Course.courses) {
+            i++;
+            System.out.println(i + ". " + course.getTitle());
+        }
     }
 }
