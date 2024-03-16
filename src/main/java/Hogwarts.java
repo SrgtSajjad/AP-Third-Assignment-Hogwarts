8f999773a39489015fbf5172578811d253e6cdf5
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-16 18:38:41 +0330

    login method updated for Hogwarts class

--- src/main/java/Hogwarts.java
+++ src/main/java/Hogwarts.java
@@ -2,6 +2,9 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Hogwarts extends Account {
+
+    Teacher hogwartsTeacher = new Teacher("Dumbledore", "1234", "teacher"); //  teacher account for accessing teacher's menu
+    Student hogwartsStudent = new Student("Jake", "1234", "student"); // student account for accessing student's menu
     public Hogwarts() {
         super("Dumbledore", "VoldemortShouldDie", "Hogwarts");
     }
@@ -12,26 +15,27 @@ public class Hogwarts extends Account {
         Hogwarts hogwarts = new Hogwarts();
         System.out.print("Username: ");
         String username = scanner.next();
-        while (true) {
-            System.out.print("Password: ");
-            String password = scanner.next();
-            if (hogwarts.validatePassword(password)) {
-                System.out.println("-Account Verified-\nWelcome " + hogwarts.getUsername());
-                break;
-            } else if (Objects.equals(password, "exit"))
-                return null;
-            System.out.println("Error: Authentication failed,Please re-enter your password\n\nType \"exit\" to leave to menu");
+        if (Objects.equals(username, hogwarts.getUsername())) {
+            while (true) {
+                System.out.print("Password: ");
+                String password = scanner.next();
+                if (hogwarts.validatePassword(password)) {
+                    System.out.println("-Account Verified-\nWelcome " + hogwarts.getUsername());
+                    break;
+                } else if (Objects.equals(password, "exit"))
+                    return null;
+                System.out.println("Error: Authentication failed,Please re-enter your password\n\nType \"exit\" to leave to menu");
+            }
+            return hogwarts;
         }
-        return hogwarts;
+        else
+            System.out.println("Error: Authentication failed,Please re-enter your username");
+        return null;
+
     }
-
-    // TODO: Define Attributes
-
-
-    // TODO: Define Functionalities
     public void viewAllTeachers() {
         int i = 0;
-        for (Teacher teacher : Educational.teachers) {
+        for (Teacher teacher : Educational.getTeachers()) {
             i++;
             System.out.println(i + ". " + teacher.getFullName());
         }
@@ -39,7 +43,7 @@ public class Hogwarts extends Account {
 
     public void viewAllStudents() {
         int i = 0;
-        for (Student student : Educational.students) {
+        for (Student student : Educational.getStudents()) {
             i++;
             System.out.println(i + ". " + student.getFullName());
         }
@@ -47,7 +51,7 @@ public class Hogwarts extends Account {
 
     public void viewAllCourses() {
         int i = 0;
-        for (Course course : Course.courses) {
+        for (Course course : Course.getCourses()) {
             i++;
             System.out.println(i + ". " + course.getTitle());
         }
